ba75b55ddf77a95eba1ede470ff6817fa5b29bb6
APHIWAT-TECH <changsaoggo123@gmail.com>
2025-11-19 19:38:51 +0700

    edit utils  nodemailer

--- back-end/src/utils/nodemailer.js
+++ back-end/src/utils/nodemailer.js
@@ -1,9 +1,20 @@
 import nodemailer from "nodemailer";
 
+
+// export const transporter = nodemailer.createTransport({
+//   service: "gmail", 
+//   auth: {
+//     user: process.env.EMAIL_USER, // Gmail account
+//     pass: process.env.EMAIL_PASS, // App Password
+//   },
+// });
+
 export const transporter = nodemailer.createTransport({
-  service: "gmail", 
+  host: process.env.SMTP_HOST,
+  port: parseInt(process.env.SMTP_PORT || "587"),
+  secure: false, // true ถ้าใช้ port 465
   auth: {
-    user: process.env.EMAIL_USER, // Gmail account
-    pass: process.env.EMAIL_PASS, // App Password
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
   },
 });
